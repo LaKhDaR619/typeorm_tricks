ed1bce06aa4fb04193a76eb8e3b480bd51aeffb3
Lakhdar Ben boudina <lakhdar.boudina@gmail.com>
2021-03-30 21:59:48 +0100

    added add, delete user

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,53 @@
 import "./App.css";
 import "antd/dist/antd.css";
+import { useEffect, useState } from "react";
 import { Button, Col, Input, Row, Table, Typography } from "antd";
-import { useState } from "react";
+import { DeleteOutlined } from "@ant-design/icons";
 
 function App() {
-  const [users, setUsers] = useState([{ name: "test", age: 2 }]);
+  const [users, setUsers] = useState([]);
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState();
+
+  const handleAddUser = async () => {
+    try {
+      if (name && age) {
+        await fetch("/users", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ name, age }),
+        });
+        getUsers();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    if (id) {
+      await fetch(`/users/${id}`, {
+        method: "DELETE",
+      });
+      getUsers();
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const res = await fetch("/users");
+      const data = await res.json();
+
+      setUsers(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   const columns = [
     {
@@ -17,6 +60,13 @@ function App() {
       dataIndex: "age",
       key: "age",
     },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <DeleteOutlined onClick={() => handleDelete(record.id)} />
+      ),
+    },
   ];
 
   return (
@@ -24,14 +74,20 @@ function App() {
       <Row gutter={[16, 16]}>
         <Col span={24}>
           <Typography style={{ width: 100 }}>Name: </Typography>
-          <Input />
+          <Input value={name} onChange={(e) => setName(e.target.value)} />
         </Col>
         <Col span={24}>
           <Typography>Age: </Typography>
-          <Input type="number" />
+          <Input
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            type="number"
+          />
         </Col>
         <Col span={24}>
-          <Button style={{ width: "100%" }}>Add User</Button>
+          <Button style={{ width: "100%" }} onClick={handleAddUser}>
+            Add User
+          </Button>
         </Col>
         <Col span={24}>
           <Table dataSource={users} columns={columns} pagination={false} />
